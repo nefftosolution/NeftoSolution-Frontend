527b5e9e34d3126c9e0fed6e78a4063e32ddbb92
nefftosolution <nefftosolution@gmail.com>
2026-07-21 13:16:45 +0500

    image updated

--- src/Components/DesignIntro.jsx
+++ src/Components/DesignIntro.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import NEFTO from '../assets/images/NEFTO.png'
+import NEFTO from '../assets/images/NEFTO.jpeg'
 
 const DesignIntro = () => {
   return (
